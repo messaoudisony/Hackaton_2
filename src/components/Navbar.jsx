d4f649db0104a847a48e97944f09dd5eaefdd10e
Alp Hamurcu <alp_hamurcu@yahoo.com>
2024-07-04 17:06:21 +0200

    button css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,28 +1,29 @@
-import { NavLink, useLocation } from 'react-router-dom';
-import styles from '../styles/components/navbar.module.scss';
-import Icon from '@mdi/react';
-import { mdiHomeCircleOutline, mdiHomeSearchOutline, mdiHelpCircleOutline} from '@mdi/js';
+import { NavLink, useLocation, useNavigate } from "react-router-dom";
+import styles from "../styles/components/navbar.module.scss";
+import Icon from "@mdi/react";
+import "./Navbar.css";
 
-const LinkNav = ({link, icon}) => {
-
+const LinkNav = ({ link, icon }) => {
   const location = useLocation();
-  const colorIcon = (link == location?.pathname) ? '#E60012' : '#00012E';
-
+  const colorIcon = link == location?.pathname ? "#E60012" : "#00012E";
   return (
     <div className={styles.linkNav}>
       <NavLink to={link}>
         <Icon path={icon} size={2} color={colorIcon} />
+        const navigate = useNavigate();
       </NavLink>
     </div>
-  )
-}
+  );
+};
 export default function Navbar() {
-
   return (
-   <div className={styles.container}>
-   <LinkNav link="/" icon={mdiHomeCircleOutline} />
-   <LinkNav link="/search" icon={mdiHomeSearchOutline} />
-   <LinkNav link="/help" icon={mdiHelpCircleOutline} />
-   </div>
+    <div className={styles.container}>
+      <NavLink className="button-accueil" to={"/"}>
+        Accueil
+      </NavLink>
+      <NavLink className="button-help" to="/help">
+        Aide
+      </NavLink>
+    </div>
   );
 }
